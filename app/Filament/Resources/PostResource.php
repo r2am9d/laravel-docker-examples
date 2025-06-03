5f652d190a8ba77d544926512470eed3a4a7eab5
Ram Delatina <r2am9d@gmail.com>
2025-06-03 17:23:03 +0800

    refactor(posts): added table filters on PostResource

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -9,6 +9,7 @@ use App\Filament\Resources\PostResource\RelationManagers\UsersRelationManager;
 use App\Models\Post;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Select;
@@ -19,11 +20,16 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Columns\CheckboxColumn;
 use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 final class PostResource extends Resource
 {
@@ -94,9 +100,12 @@ final class PostResource extends Resource
                     ->sortable()
                     ->searchable()
                     ->toggleable(),
-                CheckboxColumn::make('is_published')
+                // CheckboxColumn::make('is_published')
+                //     ->label('Published')
+                //     ->toggleable(),
+                IconColumn::make('is_published')
                     ->label('Published')
-                    ->toggleable(),
+                    ->boolean(),
                 TextColumn::make('created_at')
                     ->label('Published On')
                     ->date()
@@ -105,8 +114,37 @@ final class PostResource extends Resource
                     ->toggleable(),
             ])
             ->filters([
-                //
-            ])
+                // Filter::make('Published Posts')->query(
+                //     function (Builder $q): Builder {
+                //         return $q->where('is_published', true);
+                //     }
+                // ),
+                SelectFilter::make('category_id')
+                    ->label('Category')
+                    ->multiple()
+                    ->preload()
+                    ->relationship('category', 'name'),
+                TernaryFilter::make('is_published')
+                    ->placeholder('All')
+                    ->label('Published'),
+                Filter::make('created_at')
+                    ->form([
+                        DatePicker::make('created_from')
+                            ->label('Created From'),
+                        DatePicker::make('created_until')
+                            ->label('Created Until'),
+                    ])
+                    ->query(fn (Builder $query, array $data): Builder => $query
+                        ->when(
+                            $data['created_from'],
+                            fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                        )
+                        ->when(
+                            $data['created_until'],
+                            fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                        )),
+            ], FiltersLayout::AboveContent)
+            // ->hiddenFilterIndicators()
             ->actionsColumnLabel('Actions')
             ->actions([
                 Tables\Actions\ViewAction::make(),
