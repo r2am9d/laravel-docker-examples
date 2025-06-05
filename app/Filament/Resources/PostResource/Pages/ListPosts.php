2b6ac66c23c7c7d044302bef78f375c389209cdd
Ram Delatina <r2am9d@gmail.com>
2025-06-05 09:18:30 +0800

    refactor(posts): added tabs on listing page

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -6,7 +6,9 @@ namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 final class ListPosts extends ListRecords
 {
@@ -16,6 +18,19 @@ final class ListPosts extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make(),
+            'Published' => Tab::make()->modifyQueryUsing(function (Builder $query) {
+                return $query->where('is_published', true);
+            }),
+            'Unpublished' => Tab::make()->modifyQueryUsing(function (Builder $query) {
+                return $query->where('is_published', false);
+            }),
         ];
     }
 }
